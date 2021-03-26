6647ae1b16941618a93292dbbbe34af33b0b9383
Nadia Kennar <nakennar@gmail.com>
2021-03-26 13:14:23 +0000

    c

--- explore.R
+++ explore.R
@@ -1,9 +1,12 @@
 library(readr)
 library(dplyr)
 library(lubridate)
+library(forecast)
+library(fable)
+library(tsibble)
 
 
-calls <- read_csv("/Volumes/n8_covid/n8_data.csv.gz")
+calls <- read_csv("Z:\\n8_data_v2.csv.gz")
 
 calls %>% group_by(call_origin) %>% count() %>% View()
 
