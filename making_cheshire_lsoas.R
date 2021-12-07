b4520af460f57a6fed15743e49814d210fd42f20
maczokni <maczokni@gmail.com>
2021-12-07 16:41:26 +0000

    imd stuff

--- making_cheshire_lsoas.R
+++ making_cheshire_lsoas.R
@@ -9,7 +9,20 @@ cheshire_lsoas <- st_intersects(force_boundaries, lsoas)
 cheshire_lsoas <- lsoas[unlist(cheshire_lsoas),]
 
 plot(st_geometry(cheshire_lsoas))
-plot(st_geometry(force_boundaries),  add = T)
+plot(st_geometry(force_boundaries),  add = T, col = 'blue')
+
+
+nadia_lsoa <- st_read("/Users/reka/Downloads/nk_dis-main/data/england_lsoa_2011.shp")
+
+nadia_lsoa <- st_transform(nadia_lsoa, st_crs(force_boundaries))
+
+
+plot(st_geometry(force_boundaries), col = 'blue')
+plot(st_geometry(nadia_lsoa),  add = T)
+
+
+
+cheshire_lsoas <- cheshire_lsoas %>% filter(LSOA11CD %in% c(nadia_lsoa$code))
 
 st_write(cheshire_lsoas, "cheshire_lsoas.geojson")
 
